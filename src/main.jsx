6900666208b96db0d1605f395764e63b2ccba94b
João <57973684+WhaleJucs@users.noreply.github.com>
2025-09-17 09:54:32 -0300

    Context API em react

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { CounterContextProvider } from './context/CounterContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* 2 - criando provider */}
+    <CounterContextProvider>
+      <App />
+    </CounterContextProvider>
   </StrictMode>,
 )
