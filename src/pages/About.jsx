be0b100db14fb70c36f87148f3fcf06e44c25f0a
Joao <57973684+WhaleJucs@users.noreply.github.com>
2025-09-18 17:28:10 -0300

    Alterando contexto (variaveis globais)

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,17 @@
+import { useContext } from "react"
+import { CounterContext } from "../context/CounterContext"
+import ChangeCounter from "../components/ChangeCounter"
+
 const About = () => {
+  const { counter } = useContext(CounterContext)
+
   return (
-    <div>About</div>
+    <div>
+      <h1>Sobre</h1>
+      <p>Valor do contador: {counter}</p>
+        {/* 3 - aicionando valor ao contexto */}
+        <ChangeCounter />
+    </div>
   )
 }
 
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,6 +1,16 @@
+import { useContext } from "react"
+import { CounterContext } from "../context/CounterContext"
+import ChangeCounter from "../components/ChangeCounter"
+
 const Products = () => {
+  const { counter } = useContext(CounterContext)
+
   return (
-    <div>Products</div>
+    <div>
+      <h1>Produtos</h1>
+      <p>Valor do contador: {counter}</p>
+      <ChangeCounter />
+    </div>
   )
 }
 
