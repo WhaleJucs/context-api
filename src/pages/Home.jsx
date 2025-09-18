3c7e7ad41452f836edf73bff5184ae84c6117151
Joao <57973684+WhaleJucs@users.noreply.github.com>
2025-09-18 18:34:08 -0300

    Criando um contexto mais complexo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,23 @@
-import { useContext } from "react"
-import { CounterContext } from "../context/CounterContext"
+// import { useContext } from "react"
+// import { CounterContext } from "../context/CounterContext"
 import ChangeCounter from "../components/ChangeCounter"
+// 4 - refatorando com hook
+import { useCounterContext } from "../hooks/useCounterContext"
+// 5 - contexto mais complexo
+import { useTitleColorContext } from "../hooks/useTitleColorContext"
 
 const Home = () => {
-    const { counter } = useContext(CounterContext)
+  // const { counter } = useContext(CounterContext)
+  const { counter } = useCounterContext()
+
+  // 5 - contexto mais complexo
+  const { color } = useTitleColorContext()
+
+  console.log(color)
+
   return (
     <div>
-        <h1>Home</h1>
+        <h1 style={{ color: color }}>Home</h1>
         <p>Valor do contador: {counter}</p>
         {/* 3 - aicionando valor ao contexto */}
         <ChangeCounter />
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,10 +1,12 @@
-import { useContext } from "react"
-import { CounterContext } from "../context/CounterContext"
+// import { useContext } from "react"
+// import { CounterContext } from "../context/CounterContext"
 import ChangeCounter from "../components/ChangeCounter"
+import { useCounterContext } from "../hooks/useCounterContext"
 
 const Products = () => {
-  const { counter } = useContext(CounterContext)
-
+  // const { counter } = useContext(CounterContext)
+  const { counter } = useCounterContext()
+  
   return (
     <div>
       <h1>Produtos</h1>
